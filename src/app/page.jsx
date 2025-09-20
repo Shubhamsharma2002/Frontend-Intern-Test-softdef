a8955fee11469089db9358c45f3580471c116a40
shubham sharma <shubhamtech004@gmail.com>
2025-09-20 23:18:19 +0530

    setup

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,11 @@
+import Footer from "@/components/Footer";
 import Image from "next/image";
 
 export default function Home() {
   return (
-  <div> setup cooming soon ...</div>
+ <>
+ 
+ <Footer/>
+ </>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,20 @@
 export default function Footer() {
   return (
-  <div> setup cooming soon .         ..</div>
+  
+    <div className="bg-[#BCDDFE] text-black">
+     
+        <div className=" flex justify-around">
+        <p>E-Comm</p>
+        <p>Follow Us</p>
+        <p>Contact us</p>
+      </div>
+      <div className=" flex justify-around"> 
+        <div><p>Infomation</p></div>
+        <div><p>Service</p></div>
+        <div><p>My Account</p></div>
+        <div><p>Our Offers</p></div>
+      </div>
+      </div>
+ 
   );
 }
