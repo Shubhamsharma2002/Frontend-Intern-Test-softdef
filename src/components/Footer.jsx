8b1a16baff9c252ded339c0751896dd86d4978a5
shubham sharma <shubhamtech004@gmail.com>
2025-09-21 22:33:15 +0530

    footer section almost done with responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,7 @@
 export default function Footer() {
   return (
+
+    
     <div className="bg-[#BCDDFE] text-black flex flex-col p-10  md:p-[60px] md:gap-[70px]">
       <div className=" wid border-amber-600 border-2 flex flex-col  md:flex-row justify-between">
         <div>
