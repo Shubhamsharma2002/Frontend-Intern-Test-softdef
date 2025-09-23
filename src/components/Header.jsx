21707c8875b801ac1f14fdbdce3b5df3e92f0919
shubham sharma <shubhamtech004@gmail.com>
2025-09-23 23:43:01 +0530

    footer section almost done with responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
-export default function Header() {
-  return (
-    <div>
-        heder here..........nnnnnnnn
-    </div>
-  )
-}
+// export default function Header() {
+//   return (
+//     <div>
+//         heder here..........nnnnnnnn
+//     </div>
+//   )
+// }
